12ea86ddfba4d3cd044de3cd7b78884fe5c5de38
gaillobo <129542583+gaillobo@users.noreply.github.com>
2023-08-15 18:42:25 -0400

    Updated axios.js

--- Pawz/pawz-ui/src/axios.js
+++ Pawz/pawz-ui/src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URI || "http://localhost:3001"
+    baseURL: process.env.REACT_APP_SERVER_URI || "http://localhost:3001",
+     withCredentials: true
 });
 
 export default instance;
